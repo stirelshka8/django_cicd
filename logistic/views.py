4fafed8d6815ece3dd9f0208624f999f077bac7d
stirelshka8 <stirelshka8@gmail.com>
2023-03-17 21:37:58 +0300

    CICD

--- logistic/views.py
+++ logistic/views.py
@@ -1,7 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.response import Response
 from .models import Stock, Product
 from .serializers import ProductSerializer, StockSerializer
 
@@ -19,3 +20,7 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['products']
     search_fields = ['products__title', 'products__description']
+
+@api_view(['GET'])
+def sample_view(request):
+    return Response('Я есть ГРУТ!!!')
